905afeb5b515440e5daf5b70cb0e6cfe4ffeee69
Althaf Hussain <althafakm@gmail.com>
2017-11-18 20:03:42 +0530

    update

--- positive.py
+++ positive.py
@@ -1,14 +1,13 @@
 def main():
     i = get_positive_int()
-    print("{} is a positve integer".format(i))
+    print (" is a positive")
+    print (i,'')
 
 def get_positive_int():
     while True:
-        n = input("n is ", end="")
-
-        if n >= 1:
+        n = input("n is ")
+        if (n >= 1):
             break
     return n
-
-if __name__ == "__main()__":
+if __name__ == "__main__":
     main()
